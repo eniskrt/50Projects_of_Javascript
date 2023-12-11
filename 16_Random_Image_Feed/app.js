ae4fee8d050f23b2885ab264ebed98a041d996ff
eniskrt <enskerti@gmail.com>
2023-12-12 01:46:23 +0300

    a project added

---
+++ 16_Random_Image_Feed/app.js
@@ -0,0 +1,20 @@
+const container = document.querySelector(".container");
+const unSplashUrl = "https://source.unsplash.com/random/"
+
+const rows = 9;
+
+for(let i =0;i<rows*3; i++){
+    const img =document.createElement("img");
+    img.src =`${unSplashUrl}${getRandomSize()}`
+    container.appendChild(img)
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random()*10) + 300
+}
+// console.log(getRandomNumber());
+
+function getRandomSize() {
+    return `${getRandomNumber()}x${getRandomNumber()}`
+}
+// console.log(getRandomSize());
