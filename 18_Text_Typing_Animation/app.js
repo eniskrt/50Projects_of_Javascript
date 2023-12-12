6a1acd77fcee12a07f801a0af9303f78f33705cb
eniskrt <enskerti@gmail.com>
2023-12-12 20:54:47 +0300

    two project added

---
+++ 18_Text_Typing_Animation/app.js
@@ -0,0 +1,20 @@
+const content = document.getElementById("content");
+const speedEl = document.getElementById("speed");
+
+const text = "I'm a front-end developer and a passionate coder."
+let idx = 1;
+let speed = 300 / speedEl.value;
+
+writeText();
+
+function writeText() {
+    content.innerText = text.slice(0,idx);
+    idx++
+
+    if (idx > text.length) {
+        idx=1;
+    }
+    setTimeout(writeText,speed);
+}
+
+speedEl.addEventListener("input",(e)=>(speed = 300/e.target.value))
