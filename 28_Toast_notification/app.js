d8ee329424a076c8eeedfd52d8bea928c9b6149b
eniskrt <enskerti@gmail.com>
2023-12-27 21:02:21 +0300

    aproject added

---
+++ 28_Toast_notification/app.js
@@ -0,0 +1,22 @@
+const notifContainer = document.querySelector(".notifContainer");
+const button = document.getElementById("button");
+
+const notifications = [
+    "Instagram Bildirimi",
+    "Twitter Bildirimi",
+    "Facebook Bildirimi",
+    "WhatsApp Bildirimi",
+    "YouTube Bildirimi",
+    "Medium Bildirimi",
+    "Linkedin Bildirimi"
+];
+
+const colors = [
+    "red",
+    "blue",
+    "green",
+    "purple",
+    "black",
+    "gray",
+    "orange"
+]
