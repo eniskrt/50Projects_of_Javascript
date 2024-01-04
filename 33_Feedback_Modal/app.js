b4b3c2d1e1b8851e5f268f0d5602f40f24d166b4
eniskrt <enskerti@gmail.com>
2024-01-04 20:03:02 +0300

    last project updated

--- 33_Feedback_Modal/app.js
+++ 33_Feedback_Modal/app.js
@@ -1,13 +1,32 @@
 const ratings = document.querySelectorAll(".rating");
 const btn = document.getElementById("send");
 const panel = document.querySelector(".panel-container");
+let selectedRating = "Geri bildirim seçmediniz!"
 
-ratings.forEach(rating =>{
+ratings.forEach((rating) =>{
     rating.addEventListener("click",(e)=>{
         // console.log(e.target.parentNode);
         if(e.target.parentNode.classList.contains("rating")){
             removeActive()
             e.target.parentNode.classList.add("active")
+            // console.log(e.target.nextElementSibling.innerHTML);
+            selectedRating = e.target.nextElementSibling.innerHTML;
         }
     })
+})
+
+function removeActive() {
+    for(let i=0; i<ratings.length;i++){
+        ratings[i].classList.remove("active")
+    }
+}
+
+btn.addEventListener("click",()=>{
+    panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Geri bildirimizniz bizim için değerli.</strong>
+    <br>
+    <strong>Geri Bildiriminiz : ${selectedRating}</strong>
+    <p>Geri bildiriminizi kendimizi geliştirmek için kullanacağız.</p>
+    `
 })
